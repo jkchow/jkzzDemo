a045f97ef3415426df65210b4b5498f782c99f84
jkzzit@163.com <jkzzit@163.com>
2018-12-11 14:33:44 +0800

    first

--- src/main/java/com/jkzz/sort/HeapSort.java
+++ src/main/java/com/jkzz/sort/HeapSort.java
@@ -10,8 +10,30 @@ import java.util.Arrays;
  **/
 public class HeapSort {
 
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 5, 3, 0, 8, 6, 1, 5, 8, 6, 2, 4, 9, 4, 7, 0, 1, 8, 9, 7, 3, 1, 2, 5, 9, 7, 4, 0, 2, 6};
+        new HeapSort().sort(arr);
+    }
+
+    public int[] sort(int[] sourceArray) {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        int len = arr.length;
+
+        buildMaxHeap(arr, len);
+
+        for (int i = len - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            len--;
+            heapify(arr, 0, len);
+        }
+        return arr;
+    }
+
     private void buildMaxHeap(int[] arr, int len) {
-        for (int i = (int) Math.floor(len / 2); i > 0; i--) {
+        int i = (int) Math.floor(len / 2);
+        for (; i > 0; i--) {
             heapify(arr, i, len);
         }
     }
@@ -39,37 +61,21 @@ public class HeapSort {
     }
 
 
-    public int[] sort(int[] sourceArray) {
-        // 对 arr 进行拷贝，不改变参数内容
-        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
-
-        int len = arr.length;
-
-        buildMaxHeap(arr, len);
-
-        for (int i = len - 1; i > 0; i--) {
-            swap(arr, 0, i);
-            len--;
-            heapify(arr, 0, len);
-        }
-        return arr;
-    }
-
 }
 
 
- class HeapSort2 {
+class HeapSort2 {
 
     private int[] arr;
 
-    public HeapSort2(int[] arr){
+    public HeapSort2(int[] arr) {
         this.arr = arr;
     }
 
     /**
      * 堆排序的主要入口方法，共两步。
      */
-    public void sort(){
+    public void sort() {
         /*
          *  第一步：将数组堆化
          *  beginIndex = 第一个非叶子节点。
@@ -78,7 +84,7 @@ public class HeapSort {
          */
         int len = arr.length - 1;
         int beginIndex = (len - 1) >> 1;
-        for(int i = beginIndex; i >= 0; i--){
+        for (int i = beginIndex; i >= 0; i--) {
             maxHeapify(i, len);
         }
 
@@ -88,13 +94,13 @@ public class HeapSort {
          * 然后从新整理被换到根节点的末尾元素，使其符合堆的特性。
          * 直至未排序的堆长度为 0。
          */
-        for(int i = len; i > 0; i--){
+        for (int i = len; i > 0; i--) {
             swap(0, i);
             maxHeapify(0, i - 1);
         }
     }
 
-    private void swap(int i,int j){
+    private void swap(int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
@@ -104,17 +110,17 @@ public class HeapSort {
      * 调整索引为 index 处的数据，使其符合堆的特性。
      *
      * @param index 需要堆化处理的数据的索引
-     * @param len 未排序的堆（数组）的长度
+     * @param len   未排序的堆（数组）的长度
      */
-    private void maxHeapify(int index,int len){
+    private void maxHeapify(int index, int len) {
         int li = (index << 1) + 1; // 左子节点索引
         int ri = li + 1;           // 右子节点索引
         int cMax = li;             // 子节点值最大索引，默认左子节点。
 
-        if(li > len) return;       // 左子节点索引超出计算范围，直接返回。
-        if(ri <= len && arr[ri] > arr[li]) // 先判断左右子节点，哪个较大。
+        if (li > len) return;       // 左子节点索引超出计算范围，直接返回。
+        if (ri <= len && arr[ri] > arr[li]) // 先判断左右子节点，哪个较大。
             cMax = ri;
-        if(arr[cMax] > arr[index]){
+        if (arr[cMax] > arr[index]) {
             swap(cMax, index);      // 如果父节点被子节点调换，
             maxHeapify(cMax, len);  // 则需要继续判断换下后的父节点是否符合堆的特性。
         }
@@ -122,14 +128,16 @@ public class HeapSort {
 
     /**
      * 测试用例
-     *
+     * <p>
      * 输出：
      * [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9]
      */
     public static void main(String[] args) {
-        int[] arr = new int[]{3,5,3,0,8,6,1,5,8,6,2,4,9,4,7,0,1,8,9,7,3,1,2,5,9,7,4,0,2,6};
-        new HeapSort2(arr).sort();
-        System.out.println(Arrays.toString(arr));
+        int[] arr1 = new int[]{3, 5, 3, 0, 8, 6, 1, 5, 8, 6, 2, 4, 9, 4, 7, 0, 1, 8, 9, 7, 3, 1, 2, 5, 9, 7, 4, 0, 2, 6};
+        HeapSort2 hp2=new HeapSort2(arr1);
+        hp2.sort();
+        //new HeapSort2(arr).sort();
+        System.out.println(Arrays.toString(arr1));
     }
 
 }
