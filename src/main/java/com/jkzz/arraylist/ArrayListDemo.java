d4339527e260110c9142a230ff9595835ad35929
jkzzit@163.com <jkzzit@163.com>
2018-12-12 09:40:44 +0800

    first

--- src/main/java/com/jkzz/arraylist/ArrayListDemo.java
+++ src/main/java/com/jkzz/arraylist/ArrayListDemo.java
@@ -11,23 +11,29 @@ public class ArrayListDemo {
         int addCount = 1000000; // 这个值不要太小，否则效果不明显
         // 没有优化
         long begin1 = System.currentTimeMillis();
+        long l1 = System.nanoTime();
         for (int i = 0; i < addCount; i++) {
             list1.add(i);
         }
         long end1 = System.currentTimeMillis();
+        long l2 = System.nanoTime();
         long cost1 = end1 - begin1;
+        long cost = l2 - l1;//纳秒
 
         ArrayList<Integer> list2 = new ArrayList<>();
         // 有优化
         list2.ensureCapacity(addCount);
         long begin2 = System.currentTimeMillis();
+
+
         for (int i = 0; i < addCount; i++) {
             list2.add(i);
         }
         long end2 = System.currentTimeMillis();
         long cost2 = end2 - begin2;
 
-        System.err.println("大量 add 操作没有优化前的时间花费：" + cost1);
+        System.err.println("大量 add 操作没有优化前的时间花费：" + cost/1000);//微秒
+        System.err.println("大量 add 操作没有优化前的时间花费：" + cost1);//毫秒
         System.err.println("大量 add 操作优化后的时间花费：" + cost2);
 
     }
