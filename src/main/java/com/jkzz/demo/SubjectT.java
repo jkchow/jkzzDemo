9cbf38b2ec2c6a751cd1ba71f8552b58da6f5a83
jkzzit@163.com <jkzzit@163.com>
2018-12-24 17:30:41 +0800

    first

--- src/main/java/com/jkzz/demo/SubjectT.java
+++ src/main/java/com/jkzz/demo/SubjectT.java
@@ -26,3 +26,16 @@ public class SubjectT extends HashMap<String, Object> {
         return subject;
     }
 }
+ class VolatileExample {
+    private int a = 0;
+    private volatile boolean flag = false;
+    public void writer(){
+        a = 1;          //1
+        flag = true;   //2
+    }
+    public void reader(){
+        if(flag){      //3
+            int i = a; //4
+        }
+    }
+}
