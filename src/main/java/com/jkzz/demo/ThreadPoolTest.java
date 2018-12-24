55f9d7fd9d275083854b06ac983bed064edc8970
jkzzit@163.com <jkzzit@163.com>
2018-12-24 20:29:52 +0800

    first

---
+++ src/main/java/com/jkzz/demo/ThreadPoolTest.java
@@ -0,0 +1,45 @@
+package com.jkzz.demo;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program: jkzzDemo
+ * @description: 类描述
+ * @author: 周振
+ * @create: 2018-12-24 20:16
+ **/
+public class ThreadPoolTest {
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(5));
+        for (int i = 0; i < 21; i++) {
+            MyTask myTask = new MyTask(i);
+            executor.execute(myTask);
+            System.out.println("线程池中线程数目：" + executor.getPoolSize() + "，队列中等待执行的任务数目：" +
+                    executor.getQueue().size() + "，已执行玩别的任务数目：" + executor.getCompletedTaskCount());
+        }
+        executor.shutdown();
+    }
+}
+
+
+class MyTask implements Runnable {
+    private int taskNum;
+
+    public MyTask(int num) {
+        this.taskNum = num;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("正在执行task " + taskNum);
+        try {
+            Thread.currentThread().sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("task " + taskNum + "执行完毕");
+    }
+}
