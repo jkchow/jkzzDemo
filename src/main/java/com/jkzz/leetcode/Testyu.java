acdf2385cf32b2b6244d84c8622f4c705cf170b0
jkzzit@163.com <jkzzit@163.com>
2018-12-25 20:22:25 +0800

    first

---
+++ src/main/java/com/jkzz/leetcode/Testyu.java
@@ -0,0 +1,15 @@
+package com.jkzz.leetcode;
+
+/**
+ * @program: jkzzDemo
+ * @description: 类描述
+ * @author: 周振
+ * @create: 2018-12-25 19:51
+ **/
+public class Testyu {
+    public static void main(String[] args) {
+        if (11==2&&false||true&&1==1) {
+            System.out.println(1);
+        }
+    }
+}
