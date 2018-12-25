d086848b763da5107c706929f9718dc9203a9087
jkzzit@163.com <jkzzit@163.com>
2018-12-25 17:44:50 +0800

    first

--- src/main/java/com/jkzz/leetcode/TwoSum.java
+++ src/main/java/com/jkzz/leetcode/TwoSum.java
@@ -15,34 +15,35 @@ public class TwoSum {
     public static int[] twoSum1(int[] nums, int target) {
         HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>();
         int len = nums.length;
-        for(int i = 0; i < len; i++){
-            if(tracker.containsKey(nums[i])){
+        for (int i = 0; i < len; i++) {
+            if (tracker.containsKey(nums[i])) {
                 int left = tracker.get(nums[i]);
                 return new int[]{left, i};
-            }else{
+            } else {
                 tracker.put(target - nums[i], i);
             }
         }
         return new int[2];
     }
-    public static int[] twoSum(int[] numbers, int target) {
+
+    public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < numbers.length; i++) {
-            if (map.containsKey(target - numbers[i])) {
-                result[1] = i + 1;
-                result[0] = map.get(target - numbers[i]);
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
                 return result;
             }
-            map.put(numbers[i], i + 1);
+            map.put(nums[i], i);
         }
         return result;
     }
 
     public static void main(String[] args) {
-int[] a={1,2,3,4};
+        int[] a = {1, 2, 3, 4};
         int[] ints = twoSum(a, 5);
-        for (int c:ints) {
+        for (int c : ints) {
             System.out.println(c);
         }
     }
