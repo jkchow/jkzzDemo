08d8eb91bb7499c4015b09db9fae0be7419e024e
jkzzit@163.com <jkzzit@163.com>
2018-12-27 13:32:24 +0800

    first

--- src/main/java/com/jkzz/lambda/TestSort.java
+++ src/main/java/com/jkzz/lambda/TestSort.java
@@ -1,10 +1,8 @@
-package com.jkzz.demo;
+package com.jkzz.lambda;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
