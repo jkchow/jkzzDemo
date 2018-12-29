1cddc9ab13aa27ac13ccd3d85812f93dad1e38c9
jkzzit@163.com <jkzzit@163.com>
2018-12-29 10:28:44 +0800

    first

--- src/main/java/com/jkzz/lambda/ComparatorTest.java
+++ src/main/java/com/jkzz/lambda/ComparatorTest.java
@@ -1,5 +1,6 @@
 package com.jkzz.lambda;
 
+
 /**
  * @program: jkzzDemo
  * @description: 类描述
@@ -14,6 +15,95 @@ public class ComparatorTest {
 
 
 enum Gender {MALE, FEMALE}
-class Person{
+
+class Person {
     private String givenName;
+    private String surName;
+    private int age;
+    private Gender gender;
+    private String eMail;
+    private String phone;
+    private String address;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "givenName='" + givenName + '\'' +
+                ", surName='" + surName + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", eMail='" + eMail + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public Person() {
+    }
+
+    public Person(String givenName, String surName, int age, Gender gender, String eMail, String phone, String address) {
+        this.givenName = givenName;
+        this.surName = surName;
+        this.age = age;
+        this.gender = gender;
+        this.eMail = eMail;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
