9cb27faa2878de51db7d7810c600495ea0fbcf20
jkzzit@163.com <jkzzit@163.com>
2019-01-04 16:36:36 +0800

    first

--- src/main/java/com/jkzz/leetcode/LongestSubstr.java
+++ src/main/java/com/jkzz/leetcode/LongestSubstr.java
@@ -1,14 +1,23 @@
 package com.jkzz.leetcode;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.util.HashMap;
 
 /**
  * @program: jkzzDemo
- * @description: 获取最长子字符串
+ * @description: 不含重复字符的最长子字符串长度
  * @author: 周振
  * @create: 2019-01-01 21:46
  **/
 public class LongestSubstr {
+    public static void main(String[] args) {
+     LongestSubstr lsStr=new LongestSubstr();
+     System.out.println(lsStr.lengthOfLongestSubstring("123345678390"));
+        boolean blank = StringUtils.isBlank("\n 1");
+        System.out.println(blank);
+
+    }
     public int lengthOfLongestSubstring(String s) {
         if (s.length() == 0) return 0;
         HashMap<Character, Integer> map = new HashMap<>();
@@ -17,7 +26,7 @@ public class LongestSubstr {
             if (map.containsKey(s.charAt(i))) {
                 j = Math.max(j, map.get(s.charAt(i)) + 1);
             }
-            map.put(s.charAt(i), i);
+             map.put(s.charAt(i), i);
             max = Math.max(max, i - j + 1);
         }
         return max;
