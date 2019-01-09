341eb677c678f89a08227b8d863940877987a81b
jkzzit@163.com <jkzzit@163.com>
2019-01-09 21:59:39 +0800

    first

--- src/main/java/com/jkzz/leetcode/deleteListNode.java
+++ src/main/java/com/jkzz/leetcode/deleteListNode.java
@@ -21,8 +21,10 @@ public class deleteListNode {
     }
 
     public static ListNode buildListNode(int[] input) {
+
         ListNode first = null, last = null, newNode;
         int num;
+
         if (input.length > 0) {
             for (int i = 0; i < input.length; i++) {
                 newNode = new ListNode(input[i]);
