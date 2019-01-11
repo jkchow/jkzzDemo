8686b127e36f5c723c3878c4f53968e39187ee4b
jkzzit@163.com <jkzzit@163.com>
2019-01-11 17:32:15 +0800

    first

--- src/main/java/com/jkzz/leetcode/IntegerToRome.java
+++ src/main/java/com/jkzz/leetcode/IntegerToRome.java
@@ -12,10 +12,32 @@ public class IntegerToRome {
         String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
         String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return  M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
     }
+
+    //性能更快
+    public static String intToRoman1(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] strs = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                sb.append(strs[i]);
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
+        long l = System.nanoTime();
         String s = intToRoman(1111);
-        System.out.println(s);
+        long l2 = System.nanoTime();
+        System.out.println(l2-l);
+
+        long l3 = System.nanoTime();
+        String s1 = intToRoman1(1971);
+        long l4 = System.nanoTime();
+        System.out.println(l4-l3);
     }
 }
