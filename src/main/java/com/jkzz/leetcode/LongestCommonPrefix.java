1ad09bddfa7d19f1ab5fcf659cffe0898b380179
jkzzit@163.com <jkzzit@163.com>
2019-01-27 21:58:11 +0800

    first

--- src/main/java/com/jkzz/leetcode/LongestCommonPrefix.java
+++ src/main/java/com/jkzz/leetcode/LongestCommonPrefix.java
@@ -7,7 +7,7 @@ package com.jkzz.leetcode;
  * @create: 2019-01-27 21:44
  **/
 public class LongestCommonPrefix {
-    public String longestCommonPrefix(String[] strs) {
+    public static String longestCommonPrefix(String[] strs) {
         if(strs == null || strs.length == 0)    return "";
         String pre = strs[0];
         int i = 1;
@@ -17,5 +17,28 @@ public class LongestCommonPrefix {
             i++;
         }
         return pre;
+    }
+    public static String longestCommonPrefix1(String[] strs) {
+        if(strs == null || strs.length == 0)    return "";
+        String pre = strs[0];
+        int i = 1;
+        while(i < strs.length){
+            while(strs[i].indexOf(pre) != 0)
+                pre = pre.substring(0,pre.length()-1);
+            /**
+             * If the first cycle is completed, aimPrefix.length() == 0 is true
+             * it's mean there is no longest prefix surely
+             */
+            if (pre.length() == 0) {
+                return "";
+            } else {
+                i++;
+            }
+        }
+        return pre;
+    }
+    public static void main(String[] args) {
+        String[] abs={"12","342"};
+        longestCommonPrefix(abs);
     }
 }
