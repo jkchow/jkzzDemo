c7410ab8605172e353d9d71fdf60432946ecc909
jkzzit@163.com <jkzzit@163.com>
2019-02-26 15:12:03 +0800

    first

--- src/main/java/com/jkzz/demo/Friend.java
+++ src/main/java/com/jkzz/demo/Friend.java
@@ -34,6 +34,16 @@ public class Friend {
         this.age = age;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return nickname.hashCode()+age;
+    }
+
     public Friend() {
     }
 
@@ -55,9 +65,13 @@ public class Friend {
         set.add(p2);
         set.add(p3);
         System.out.println("总共有:" + set.size() + " 个元素!"); //结果：总共有:3 个元素!
+        for (Friend person : set) {
+            System.out.println(person);
+        }
+        System.out.println("______");
         p3.setAge(2); //修改p3的年龄,此时p3元素对应的hashcode值发生改变
         set.remove(p3); //此时remove不掉，造成内存泄漏
-        set.add(p3); //重新添加，居然添加成功
+        //set.add(p3); //重新添加，居然添加成功
         System.out.println("总共有:" + set.size() + " 个元素!"); //结果：总共有:4 个元素!
         for (Friend person : set) {
             System.out.println(person);
